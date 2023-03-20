0e285597234070739475f684a9074929cccf5a0a
C2RUN <jose.cerda66@gmail.com>
2023-03-20 11:55:42 -0300

    Add files via upload
    Primeros cambios

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          var paisesUE = new List<string>() {"España", "Austria", "Italia", "Alemania", "Dinamarca","Suecia", "Finlandia"};
+          var paisesUEL = new List<string>();
+          
+          //recorro todos los paises de la lista
+          foreach(var p in paisesUE)
+          {
+            //verifico si la longitud es mayor a 6 caracteres y añado a la lista p
+            if(p.Length > 6)
+            paisesUE.Add(p);
+          }
+        //se puede hacer lo mismo con linq de manera reducida
+        var paisesUELLinq = paisesUE.Where(p => p.Length > 6);
+        }
+    }
+}
